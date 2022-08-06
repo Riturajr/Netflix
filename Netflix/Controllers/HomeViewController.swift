49c07949b0539790acd409c10a465cd5421c9aed
Rituraj <riturajnambiar98@gmail.com>
2022-08-06 13:06:50 +0530

    Configured APICalls .

--- Netflix/Controllers/HomeViewController.swift
+++ Netflix/Controllers/HomeViewController.swift
@@ -32,7 +32,7 @@ class HomeViewController: UIViewController {
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
         
-        getTrendingMovies()
+       fetchData()
         
     }
     
@@ -54,16 +54,30 @@ class HomeViewController: UIViewController {
         homeFeedTable.frame = view.bounds
     }
     
-    private func getTrendingMovies() {
-        APICaller.shared.getTrendingMovies { results in
-            switch results {
-            case .success(let movies):
-                print(movies)
-            case .failure(let error):
-                print(error)
-            }
+    private func fetchData() {
+//        APICaller.shared.getTrendingMovies { results in
+//            switch results {
+//            case .success(let movies):
+//                print(movies)
+//            case .failure(let error):
+//                print(error)
+//            }
+//        }
+//        APICaller.shared.getTrendingTvs { results in
+//
+//        }
+        
+//        APICaller.shared.getUpcomingMovies { results in
+//
+//        }
+//        APICaller.shared.getPopular { reults  in
+//
+//        }
+        APICaller.shared.getToprated { results in
+            
         }
     }
+
 
 }
 
---
+++ Netflix/Resources/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  Netflix
+//
+//  Created by Rituraj on 06/08/22.
+//
+
+import Foundation

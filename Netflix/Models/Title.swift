daee2937060faa861a355633a866ae59b716d81c
Rituraj <riturajnambiar98@gmail.com>
2022-08-06 17:19:43 +0530

    Fetch poster images and display within collectionView cells.

--- Netflix/Models/Title.swift
+++ Netflix/Models/Title.swift
@@ -1,5 +1,5 @@
 //
-//  Tv.swift
+//  Movie.swift
 //  Netflix
 //
 //  Created by Rituraj on 06/08/22.
@@ -7,11 +7,11 @@
 
 import Foundation
 
-struct TrendingTvResponse: Codable {
-    let results: [Tv]
+struct TrendingTitleResponse: Codable {
+    let results: [Title]
 }
 
-struct Tv: Codable {
+struct Title: Codable {
     let id: Int
     let media_type: String?
     let original_name: String?

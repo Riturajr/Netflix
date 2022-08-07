7807aec9afd511c8ebaf634ac12a722c1d5efc27
Rituraj <riturajnambiar98@gmail.com>
2022-08-07 07:42:35 +0530

    Configure Upcoming ViewController.

---
+++ Netflix/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix
+//
+//  Created by Rituraj on 07/08/22.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
